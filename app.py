ba51516217fbff2efe1876e9d7d0589d263f34e5
arasorhun <arasorhun@yahoo.com>
2025-04-18 21:11:22 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -65,4 +65,9 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+)
